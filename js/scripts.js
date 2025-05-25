53b8088ddc7b776486ee47333fb06f761363ed79
ideali19 <razoarimendrikadilanidealisain@gmail.com>
2025-05-25 15:57:51 +0000

    Add mon-portfolio

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Fonction de défilement fluide
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
